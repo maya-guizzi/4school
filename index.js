a3dc24220c4d97ec71edd16d4ef3b9612be15677
maya-guizzi <maya.guizzi@student.h-is.com>
2022-04-04 15:08:00 +0200

    c

---
+++ index.js
@@ -0,0 +1,33 @@
+// Dependencies
+const express = require('express')
+const mongoose = require('mongoose')
+const bodyParser = require('body-parser');
+
+
+// create the app
+const app =  express()
+
+// enable POST request
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+// Install ejs - to show JS variables into the HTML
+app.set('view engine','ejs')
+
+// Connect to mongoDb
+const mongoDbURI = 'mongodb+srv://mayaguizzi:4school@cluster0.hqum5.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+mongoose.connect(mongoDbURI)
+
+
+// Connect the Models
+require('./models/Article')
+
+
+// Connect the routes
+app.use(require('./routes/frontend'))
+
+
+const frontendRoutes = require('./routes/frontend')
+
+
+app.listen(3000)
