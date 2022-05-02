28fa87249bd1dd7388ec90e1782f4dc8390d2aa8
NickFrigerio <nicolo.frigerio@student.h-is.com>
2022-05-02 12:45:04 +0200

    login (to immprove)
    e

---
+++ routes/auth.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const mongoose = require('mongoose')
+
+const User = mongoose.model('users')
+
+const router = express.Router()
+
+router.get('/login', async (req,res) => {
+    // render the list of articles
+    const users = await User.find().lean()
+  console.log(users)
+    res.render('login.ejs', {
+        users:users
+    })
+  })
+
+module.exports = router;
