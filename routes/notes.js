4b99914dff6d483d6b39828ec86094caabfba2e8
maya-guizzi <maya.guizzi@student.h-is.com>
2022-05-02 13:59:49 +0200

    a

---
+++ routes/notes.js
@@ -0,0 +1,25 @@
+// dependencies 
+const express = require('express')
+const mongoose = require('mongoose')
+
+// Get the models
+const Notes = mongoose.model('notes')
+const Forum = mongoose.model('forum')
+
+
+// create the routes
+const router = express.Router()
+
+// create the request
+//list of the articles
+router.get('/notes', async (req,res) => {
+  // render the list of articles
+  const notes = await Notes.find().lean()
+  console.log(notes)
+  res.render('notes', {
+    notes:notes
+  })
+})
+
+// export the requests
+module.exports = router;
